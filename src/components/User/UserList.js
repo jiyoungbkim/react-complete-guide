ac6a87c99f92c8db1af7ce29d6da821bc3f9bb65
jiyoungbkim <jiyoungbkim@gmail.com>
2022-01-16 20:20:20 +0900

    95-97 : AddUser, Card

--- src/components/User/UserList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./UserList.css";
-
-const UserList = (props) => {
-  return <ul className="user-list"></ul>;
-};
-
-export default UserList;
--- src/components/UserApp.js
+++ src/components/UserApp.js
@@ -1,19 +1,12 @@
 import React, { useState } from "react";
-import UserInput from "./User/UserInput";
-import "./UserApp.css";
+
+import AddUser from "./Users/AddUser";
 
 const UserApp = () => {
-  let content = (
-    <p style={{ textAlign: "center" }}>No user found. Maybe add one?</p>
-  );
-
   return (
-    <>
-      <section id="user-form">
-        <UserInput label="Username" />
-      </section>
-      <section id="users">{content}</section>
-    </>
+    <div>
+      <AddUser />
+    </div>
   );
 };
 
